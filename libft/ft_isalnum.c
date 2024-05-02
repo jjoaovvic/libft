e96f724beb37eefa14655dab970e0f8353c8c15e
jjoaovvic <31035129+jjoaovvic@users.noreply.github.com>
2024-05-02 15:40:15 -0300

    Update ft_isalnum.c

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -2,11 +2,11 @@ int	ft_isalnum(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
-		return (8);
+		return (1);
 	}
 	else if (c >= '0' && c <= '9')
 	{
-		return (8);
+		return (1);
 	}
 	else
 		return (0);
